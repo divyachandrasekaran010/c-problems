9aaa7ccbae55fd7bac6482e73441d19976c4363f
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 14:45:54 +0530

    Add files via upload

---
+++ print variable in  int data type .c
@@ -0,0 +1,19 @@
+Print int variable
+Test Case 1
+Output: 10
+Test Case 2
+
+Output: 99
+
+program:
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",a);
+//test case passes
+int c=10;
+int b=99;
+printf("%d\n%d",c,b);
+return 0;
+}
