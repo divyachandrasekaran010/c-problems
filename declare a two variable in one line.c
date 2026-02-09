51b00a3c6e9f47a732bbf51983721cf3327504a3
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 12:09:41 +0530

    Add files via upload

---
+++ declare a two variable in one line.c
@@ -0,0 +1,16 @@
+Declare two integer variables and print them
+Test Case 1
+Output: 5 15
+Test Case 2
+Output: 20 40
+
+
+
+#include<stdio.h>
+int main(){
+    int a=5,b=15;
+    int a2=20,b2=40;
+    printf("%d %d\n",a,b);
+    printf("%d %d",a2,b2);
+    return 0;
+}
