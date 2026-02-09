96b313d8500a1c996f226bd305702cc97e6719a0
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 13:37:13 +0530

    Add files via upload

---
+++ add two variables.c
@@ -0,0 +1,15 @@
+Add two variables
+Test Case 1
+Output: 15
+Test Case 2
+Output: 30
+
+program:
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int sum=a+b;
+    printf("%d",sum);
+  return 0;
+}
