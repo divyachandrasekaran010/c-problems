300b556f8161c91e2e4ec7b71f1375fce523aa82
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 12:18:39 +0530

    Add files via upload

---
+++ declare a character variable.c
@@ -0,0 +1,15 @@
+Declare a character variable and print it
+Test Case 1
+Output: X
+Test Case 2
+Output: B
+
+program:
+#include<stdio.h>
+int main(){
+    char a='X';
+    char b='B';
+    printf("%c\n",a);
+    printf("%c",b);
+    return 0;
+}
