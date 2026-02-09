1e838223ab9c562210eef4baa00344b99312add3
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 16:26:32 +0530

    Add files via upload

---
+++ store char in int .c
@@ -0,0 +1,15 @@
+Store char in int
+Test Case 1
+Output: 65
+Test Case 2
+Output: 97
+
+program:
+#include<stdio.h>
+int main(){
+char a='A';
+char b='a';
+printf("%d\n", a);
+printf("%d\n",b);
+    return 0;
+}
