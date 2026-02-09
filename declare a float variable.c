24eb68dfa1b244dd9e799de9c063a0a49088c0c5
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 12:13:22 +0530

    Add files via upload

---
+++ declare a float variable.c
@@ -0,0 +1,15 @@
+Declare a float variable and print it
+Test Case 1
+Output: 2.5
+Test Case 2
+Output: 6.75
+
+program:
+#include<stdio.h>
+int main(){
+    float a=2.5;
+    float b=6.75;
+    printf("%.1f\n",a);
+    printf("%.2f",b);
+    return 0;
+}
