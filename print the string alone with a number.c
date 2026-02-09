939b0d0c543d210d49e52868495e8b843dff1e2a
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 11:44:27 +0530

    Add files via upload

---
+++ print the string alone with a number.c
@@ -0,0 +1,15 @@
+Print a string along with a number
+Test Case 1
+Output: Age: 20
+Test Case 2
+Output: Count: 5
+
+
+#include<stdio.h>
+int main(){
+    int age=20;
+    int count=5;
+    printf(" Age:%d\n",age);
+    printf(" Count:%d",count);
+    return 0;
+}
