cb01d307dc6b5ab375510ff108284bc2894452be
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 11:49:29 +0530

    Add files via upload

---
+++ prinnt output in two lines.c
@@ -0,0 +1,17 @@
+Print output in two lines using \n
+Test Case 1
+Output:
+Hello
+World
+Test Case 2
+Output:
+Good
+Morning
+
+program:
+#include<stdio.h>
+int main(){
+    printf("Hello\nWorld\n");
+    printf("Good\nMorning");
+    return 0;
+}
