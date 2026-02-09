ef913d6f14dd5c28e0cd969bbc2c22a4d19bd733
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 11:07:58 +0530

    Add files via upload

---
+++ print the integer 2.c
@@ -0,0 +1,15 @@
+Print an integer value
+Test Case 1
+Output: 10
+Test Case 2
+Output: 25
+
+program:
+#include<stdio.h>
+int main(){
+    int a=10;
+     int b=25;
+    printf("%d\n",a);
+    printf("%d",b);
+    return 0;
+}
