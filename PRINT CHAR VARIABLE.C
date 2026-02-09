b9850d840a75833a4a266f8d0b6153a876834476
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-09 15:33:25 +0530

    Add files via upload

---
+++ PRINT CHAR VARIABLE.C
@@ -0,0 +1,14 @@
+Print char variable
+Test Case 1
+Output: A
+Test Case 2
+Output: M
+
+PROGRAM:
+#include<stdio.h>
+int main(){
+    char a='A';
+    char b='M';
+    printf("%c\n%c",a,b);
+    return 0;
+}
