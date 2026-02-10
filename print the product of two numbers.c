8d45294f7c9e046bf9276f9b8bf6ac67258586d2
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-10 11:34:39 +0530

    Add files via upload

---
+++ print the product of two numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Input:");
+    scanf("%d %d",&a,&b);
+    int diff=a-b;
+    printf("%d",diff);
+    return  0;
+}
+
+program:
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Input:");
+    scanf("%d %d",&a,&b);
+    int product=a*b;
+    printf("%d",product);
+    return  0;
+}
