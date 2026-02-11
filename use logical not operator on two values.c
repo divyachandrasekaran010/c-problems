af11f6bdf4a66b5fcc020265dc6fdd0e46ee6b7e
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-11 13:29:01 +0530

    Add files via upload

---
+++ use logical not operator on two values.c
@@ -0,0 +1,21 @@
+Apply logical NOT on zero.
+Test Case 1
+Input: 0
+Output: 1
+Test Case 2
+Input: 0
+Output: 1
+Test Case 3
+Input: 0
+Output: 1
+ 
+program:
+
+#include <stdio.h>
+int main() {
+    int a;
+    printf("input:",a);
+    scanf("%d",&a);
+    printf("%d",!a);
+    return 0;
+}
