1d2aaa0102b90bdb984751c0334e2cf57a987764
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-11 13:45:58 +0530

    Add files via upload

---
+++ Assign a value and print it.c
@@ -0,0 +1,20 @@
+Assign a value and print it.
+Test Case 1
+Input: 10
+Output: 10
+Test Case 2
+Input: 5
+Output: 5
+Test Case 3
+Input: 0
+Output: 0
+
+
+#include <stdio.h>
+int main() {
+    int a;
+    printf("input:");
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0;
+}
