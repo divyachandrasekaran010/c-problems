aecbdfbe6effee3a146241b903a108f683132395
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-11 13:24:02 +0530

    Add files via upload

---
+++ use logical or operator on two values.c
@@ -0,0 +1,11 @@
+use logical or operator on two values:
+
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("input:");
+    scanf("%d %d",&a,&b);
+    int c=a||b;
+    printf("%d",c);
+    return 0;
+}
