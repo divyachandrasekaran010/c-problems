e5139b086dcbc855c625c177a23201539c266c45
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-21 14:22:52 +0530

    Add files via upload

---
+++ Fuel Tank Safety Consumption Monitor (While Loop).c
@@ -0,0 +1,26 @@
+Fuel Tank Safety Consumption Monitor (While Loop)
+Input
+50
+5
+10 15 20 10 5
+Output
+Completed Trips: 3
+Remaining Fuel: 5
+
+#include <stdio.h>
+int main() {
+    int fuel, N;
+    scanf("%d%d", &fuel, &N);
+    int use, trips = 0, i = 0;
+    while(i < N) {
+        scanf("%d", &use);
+        if(fuel < use)
+            break;
+        fuel -= use;
+        trips++;
+        i++;
+    }
+    printf("Completed Trips: %d\n", trips);
+    printf("Remaining Fuel: %d\n", fuel);
+    return 0;
+}
