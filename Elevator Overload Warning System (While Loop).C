a7e88afdc6bc3b9f450003ca924ee73e72086d20
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-02-21 13:15:34 +0530

    Add files via upload

---
+++ Elevator Overload Warning System (While Loop).C
@@ -0,0 +1,19 @@
+Elevator Overload Warning System (While Loop)
+Using a while loop, determine how many passengers entered before overload.
+#include <stdio.h>
+int main() {
+    int maxWeight, N;
+    scanf("%d%d", &maxWeight, &N);
+    int weight, total = 0, count = 0, i = 0;
+    while(i < N) {
+        scanf("%d", &weight);
+        if(total + weight > maxWeight)
+            break;
+        total += weight;
+        count++;
+        i++;
+    }
+    printf("Passengers Allowed: %d\n", count);
+    printf("Overload: %s\n", (count < N) ? "Yes" : "No");
+    return 0;
+}
