88d064b984d306eef3691b3f376dac33338479ae
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-03-10 12:07:13 +0530

    Add files via upload

---
+++ Department Consistency Check.C
@@ -0,0 +1,29 @@
+Department Consistency Check
+Test Case
+Input
+ 3 3
+ 3000 3200 3100
+ 1000 2500 4000
+ 5000 5200 5100
+Output
+ 2
+ 
+#include <stdio.h>
+int main(){
+    int R,C,count=0;
+    scanf("%d%d",&R,&C);
+    int a[R][C];
+    for(int i=0;i<R;i++)
+        for(int j=0;j<C;j++)
+            scanf("%d",&a[i][j]);
+    for(int i=0;i<R;i++){
+        int max=a[i][0],min=a[i][0];
+        for(int j=1;j<C;j++){
+            if(a[i][j]>max) max=a[i][j];
+            if(a[i][j]<min) min=a[i][j];
+        }
+        if(max-min <= 1000)
+            count++;
+    }
+    printf("%d",count);
+}
