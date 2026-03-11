41b7c4b2919dea48a90b37774a1973dfdfee15d5
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-03-11 15:17:11 +0530

    Add files via upload

---
+++ Vowel Counter in Reviews-string6.C
@@ -0,0 +1,23 @@
+Vowel Counter in Reviews
+
+Test Cases
+Input: Amazing → 4
+ Explanation: A,a,i → 4
+Input: Sky → 0
+Input: Education → 5
+
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i = 0, count = 0;
+    scanf("%s", str);
+    while(str[i] != '\0') {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' ||
+           str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U') {
+            count++;
+           }
+        i++;
+    }
+    printf("%d", count);
+    return 0;
+}
