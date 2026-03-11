a51f4df06a62dd524a77ce79a7452c004ed4fc04
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-03-11 14:42:24 +0530

    Add files via upload

---
+++ Reverse a Customer Feedback-string 4.c
@@ -0,0 +1,21 @@
+Reverse a Customer Feedback
+
+Test Cases
+Input: good
+ Output: doog
+Input: excellent
+ Output: tnellecxe
+Input: AI
+ Output: IA
+ 
+#include<stdio.h>
+int main(){
+    char str[100];
+    int i,len=0;
+    scanf("%s",str);
+    while(str[len]!='\0')
+        len++;
+    for(i=len-1;i>=0;i--)
+        printf("%c",str[i]);
+    return 0;
+}
