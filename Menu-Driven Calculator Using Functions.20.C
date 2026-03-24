e787175491d6dad72229577d2eea1a210b506c6b
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-03-24 11:19:47 +0530

    Add files via upload

---
+++ Menu-Driven Calculator Using Functions.20.C
@@ -0,0 +1,42 @@
+Menu-Driven Calculator Using Functions
+
+#include <stdio.h>
+int add(int a, int b)
+{
+return a + b;
+}
+int sub(int a, int b)
+{
+return a - b;
+}
+int mul(int a, int b)
+{
+return a * b;
+}
+float divi(int a, int b)
+{
+return (float)a / b;
+}
+int main()
+{
+int a, b;
+char op;
+scanf("%d %d %c", &a, &b, &op);
+switch(op)
+{
+case '+':
+printf("%d", add(a,b));
+
+break;
+case '-':
+printf("%d", sub(a,b));
+break;
+case '*':
+printf("%d", mul(a,b));
+break;
+case '/':
+printf("%.2f", divi(a,b));
+break;
+}
+return 0;
+}
