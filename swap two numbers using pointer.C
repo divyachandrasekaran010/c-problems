b29108649dd4dacd998a62ba0a9337ba833c3112
divyachandrasekaran010 <divyachandrasekaran010@gmail.com>
2026-03-25 13:44:28 +0530

    Add files via upload

---
+++ swap two numbers using pointer.C
@@ -0,0 +1,19 @@
+swap two number using pointer
+
+
+#include <stdio.h>
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("Before swap: %d %d\n", a, b);
+    swap(&a, &b);
+    printf("After swap: %d %d", a, b);
+    return 0;
+}
